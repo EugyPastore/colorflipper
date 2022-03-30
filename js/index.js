4a38b93a5910e22547e9ec1f1e4f9627e6f2db8b
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-03-30 21:21:52 +0200

    added alternative function for simple

--- js/index.js
+++ js/index.js
@@ -3,14 +3,10 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 // 2 targets button to click and text
 const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
-// event listener
 btn.addEventListener('click', function () {
 	// get random number array indexes 0-3 colors[0], colors[1],colors[2], colors[3]
 
 	const randomNumber = getRandomNumber();
-	console.log(randomNumber);
-
-
 	document.body.style.backgroundColor = colors[randomNumber];
 	color.textContent = colors[randomNumber];
 });
@@ -18,3 +14,13 @@ btn.addEventListener('click', function () {
 function getRandomNumber() {
 	return Math.floor(Math.random() * colors.length);
 }
+
+// Second way of writing same function
+// btn.onclick = function () {
+// 	console.log("checking out functions")
+
+// 	const randomNumber = Math.floor(Math.random() * colors.length);
+// 	color.innerText = colors[randomNumber];
+// 	console.log(randomNumber);
+// 	document.body.style.backgroundColor = colors[randomNumber]
+// };
